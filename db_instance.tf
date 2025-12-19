1b411d7efca0f5624b114ffdb6ac838f1c02d252
Pramod gaikwad <gaikwadgpramod369@gmail.com>
2025-12-19 08:18:26 +0530

    aa

--- db_instance.tf
+++ db_instance.tf
@@ -1,10 +1,10 @@
 resource "aws_db_instance" "db_instance" {
   engine                   = var.engine_name
-  name                     = var.db_name
+  identifier               = var.db_name
   username                 = var.user_name
   password                 = var.pass
   skip_final_snapshot      = var.skip_finalSnapshot
-  db_subnet_group_name     = aws_db_subnet_group.db_sub_group.id
+  db_subnet_group_name     = aws_db_subnet_group.db_sub_group.name
   delete_automated_backups = var.delete_automated_backup
   multi_az                 = var.multi_az_deployment
   publicly_accessible      = var.public_access
